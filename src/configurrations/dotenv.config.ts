a836e3bbf0f16de45dceb47cd4b6472ed2cb6510
Alex <139467897+Alex-LaNN@users.noreply.github.com>
2024-10-12 22:15:09 +0300

    Removing extra line in dotenv.config.ts file

--- src/configurrations/dotenv.config.ts
+++ src/configurrations/dotenv.config.ts
@@ -1,5 +1,3 @@
-// import 'dotenv/config'
-
 const dotenvFlow = require('dotenv-flow')
 
 // Initialize 'dotenv-flow' to load the appropriate .env file (When using 2 different .env files in a project for 'dev' and 'prod')
